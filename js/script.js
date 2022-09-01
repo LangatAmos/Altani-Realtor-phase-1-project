e2d498787addc3dc3d7cee123717debdda510e32
Amos Langat <amos.langat@student.moringaschool.com>
2022-09-01 12:17:58 +0300

    add sticky head event onscoll

--- js/script.js
+++ js/script.js
@@ -1,17 +1,39 @@
+// fetch data from the server
+document.addEventListener('DOMContentLoaded', function() {
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '224e070ea7mshfb00fdc5e69b177p1b58b3jsn20f89408b922',
+		'X-RapidAPI-Host': 'realty-in-ca1.p.rapidapi.com'
+	}
+};
+	function fetchData(){
+    return fetch('https://realty-in-ca1.p.rapidapi.com/properties/list-residential?LatitudeMax=81.14747595814636&LatitudeMin=-22.26872153207163&LongitudeMax=-10.267941690981388&LongitudeMin=-136.83037765324116&CurrentPage=1&RecordsPerPage=10&SortOrder=A&SortBy=1&CultureId=1&NumberOfDays=0&BedRange=0-0&BathRange=0-0&RentMin=0', options)
+	.then(response => response.json())
+	.then((data) => {
+        //return renderProperty(response);
+        console.log(data);
+    })
+	.catch(err => console.error(err));
+
+}
+console.log(fetchData());
+
+
+//creating sticky navbar 
 const header = document.querySelector(".header");
 const sticky = header.offsetTop;
-
-function headerStick() {
+window.addEventListener("scroll", function () {
     if (window.scrollY >= sticky) {
         header.classList.add("sticky")
     } else {
         header.classList.remove("sticky");
     }
-}
+});
 
  // toggle between element classes
-
-$(".menu-btn").click(function () {
-    $(".navbar .nav-links").toggleClass("active");
-    $(".menu-btn i").toggleClass("active");
+const toggle = document.addEventListener("click", function () {
+	(".navbar .nav-links").toggleClass("active");
+    (".menu-btn i").toggleClass("active");
+});
 });
